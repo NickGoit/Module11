9f25030310e2cc8a430dea1a061b17254d181acc
NickGoit <113237474+NickGoit@users.noreply.github.com>
2023-04-01 11:56:34 +0300

    Models to db

--- src/database/models.py
+++ src/database/models.py
@@ -1,17 +1,17 @@
-from sqlalchemy import Column, Integer, String, Boolean, func, DATE
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy import Column, Integer, String, func, DATE
 from sqlalchemy.sql.sqltypes import DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class Contact(Base):
     __tablename__ = "contact"
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False)
-    last_name = Column(String(50), nullable=False)
-    phone = Column(String(50), nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
     date_of_birth = Column(DATE)
     created_at = Column('created_at', DateTime, default=func.now())
     updated_at = Column('created_at', DateTime, default=func.now())
